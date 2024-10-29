dfb62467b09339591c5c8c1418e967fafef2d04c
Dorji/image6 <dorjilhamo191@gmail.com>
2024-10-29 22:28:37 +0600

    commit

---
+++ FUNCTIONS_AND _SCOPE/functions_and_scope/basic_functions.py
@@ -0,0 +1,15 @@
+def greet():
+    print("Hello, World!")
+
+greet()
+
+def greet(name):
+    print(f"Hello, {name}!")
+
+greet("Alice")
+
+def square(number):
+    return number ** 2
+
+result = square(5)
+print(result)
